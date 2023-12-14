debcfdae4712500dcc5a7a2ceebb16af9cda6261
gloriadukuzeyesu <gloriadukuzeyesu@gmail.com>
2023-12-14 16:55:09 -0700

    day 13 part 2

---
+++ AdventOfCode_python/Year2023/Day13/part2.py
@@ -0,0 +1,30 @@
+# find the mirror
+ 
+def find_mirror (grid):
+    for r in range(1, len(grid)):
+        above = grid[:r][::-1]
+        below = grid[r:]
+
+        # above = above[:len(below)]
+        # below = below[:len(above)]
+
+        if sum( sum( 0 if a == b else 1 for a, b in zip(x,y)) for x, y in zip(above, below)) == 1:
+            return r  
+    return 0
+
+total = 0
+
+with open("Day13/input.txt", "r") as file:
+    data = file.read().split("\n\n")
+    # print(data)
+
+
+for block in data:
+    grid = block.splitlines()
+    row = find_mirror(grid)
+    total += row * 100
+    col = find_mirror(list(zip(*grid)))
+    total += col
+
+  
+print(total)
